99d3de9ddaec501402033ad553b483dd568ed8e1
vladimir-grigoriev <v_s_grigoriev@mail.ru>
2021-07-09 13:51:48 +0300

    reformatted the modules with tests

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.remote.webelement import WebElement
 LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_button(browser):
     browser.get(LINK)
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
     assert isinstance(button, WebElement), "Button not found"
