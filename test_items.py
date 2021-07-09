3287919590926cd9a1561544c6e262faec739399
vladimir-grigoriev <v_s_grigoriev@mail.ru>
2021-07-09 13:52:53 +0300

    reformatted the modules with tests

--- test_items.py
+++ test_items.py
@@ -8,4 +8,5 @@ def test_guest_should_see_button(browser):
     browser.get(LINK)
     button = browser.find_element_by_css_selector(".btn-add-to-basket")
     assert isinstance(button, WebElement), "Button not found"
-    time.sleep(5)
+    print(button.text)
+    time.sleep(30)
